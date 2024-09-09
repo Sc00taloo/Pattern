94f2d48223c37b89445b24aa45cad9f5c34d3205
Sc00taloo <v4nyapop0v12@gmail.com>
2024-09-09 16:32:49 +0300

    Task 3 - Избежать дублирования кода в конструкторе, геттере и сеттере.
    Вроде как избежал. Убрал геттеры и сеттеры, так как kotlin автоматом предоставляет методы для объектов

--- LR1 Core classes and models/main/src/Student.kt
+++ LR1 Core classes and models/main/src/Student.kt
@@ -1,43 +1,6 @@
 package main.src
 
-class Student(private val id: Int, private var lastName: String, private var firstName: String, private var middleName: String, private var phone: String? = null, private var telegram: String? = null, private var email: String? = null, private var git: String? = null){
-    fun getID(): Int = id
-
-    fun getLastName(): String = lastName
-    fun setLastName(value: String) {
-        lastName = value
-    }
-
-    fun getFirstName(): String = firstName
-    fun setFirstName(value: String) {
-        firstName = value
-    }
-
-    fun getMiddleName(): String = middleName
-    fun setMiddleName(value: String) {
-        middleName = value
-    }
-
-    fun getPhone(): String? = phone
-    fun setPhone(value: String?) {
-        phone = value
-    }
-
-    fun getTelegram(): String? = telegram
-    fun setTelegram(value: String?) {
-        telegram = value
-    }
-
-    fun getEmail(): String? = email
-    fun setEmail(value: String?) {
-        email = value
-    }
-
-    fun getGit(): String? = git
-    fun setGit(value: String?) {
-        git = value
-    }
-
+class Student(val id: Int, var lastName: String, var firstName: String, var middleName: String, var phone: String? = null, var telegram: String? = null, var email: String? = null, var git: String? = null){
     fun getFullName(): String {
         return "$lastName $firstName $middleName".trim()
     }
