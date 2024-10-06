291cf39a161f82af9f70640fed96ee3541d142df
Sc00taloo <v4nyapop0v12@gmail.com>
2024-10-07 01:43:45 +0300

    Task 1.1 - Написать отдельный конструктор, принимающий на вход строку, который эту строку парсит и вызывает стандартный конструктор с параметрами

---
+++ LR2 Reading Class models/src/Main.kt
@@ -0,0 +1,85 @@
+import main.src.Student
+
+fun main() {
+//    val student1 = Student(
+//        id = 1,
+//        lastName = "Иван",
+//        firstName = "Попов",
+//        middleName = "Викторович",
+//        phone = "+78005553535",
+//        git = "https://github.com/Sc00taloo"
+//    )
+//    val student2 = Student(
+//        id = 2,
+//        lastName = "Николай",
+//        firstName = "Палыч",
+//        middleName = "Победоносный",
+//        email = "sobaka12@gmail.com",
+//        git = "https://github.com/SlivkiShow"
+//    )
+//    val student3 = Student(
+//        id = 3,
+//        lastName = "Денисова",
+//        firstName = "Анастасия",
+//        middleName = "Давидовна",
+//        telegram = "@denis",
+//        git = "https://github.com/Denisochka"
+//    )
+//    val student4 = Student(
+//        id = 4,
+//        lastName = "Пономарёв",
+//        firstName = "Максим",
+//        middleName = "Фёдорович",
+//        phone = "+79451239009",
+//        telegram = "@pomafyo123",
+//        email = "pomafyo123@mail.ru",
+//        git = "https://github.com/Killer2016"
+//    )
+//    val student5 = Student(
+//        id = 5,
+//        lastName = "Моисеева",
+//        firstName = "Арина",
+//        middleName = "Арсентьева",
+//        phone = "+79451239009",
+//        telegram = "@moise11",
+//        git = "https://github.com/TrueProg"
+//    )
+//
+//    val student6 = Student(hashMapOf(
+//        Pair("id", 6),
+//        Pair("lastName", "Косян"),
+//        Pair("firstName", "Артём"),
+//        Pair("middleName", "Власович"),
+//        Pair("phone","+79451239009"),
+//        Pair("git","https://github.com/Github")
+//    ))
+//    println(student1)
+//    println(student1.validate())
+//    println(student2)
+//    println(student2.validate())
+//    println(student3)
+//    println(student3.validate())
+//    println(student4)
+//    println(student4.validate())
+//    println(student5)
+//    println(student5.validate())
+//    println(student6)
+//    println(student6.validate())
+//
+//    student2.set_contacts(email = "nikolai1970@mail.ru")
+//    student5.set_contacts(telegram = "@hackmoise11")
+//    println(student2)
+//    println(student2.validate())
+//    println(student5)
+//    println(student5.validate())
+//
+//    try {
+//        student1.set_contacts(phone = "+73124")
+//    } catch (e: IllegalArgumentException) {
+//        println("Ошибка: ${e.message}")
+//    }
+
+    val studentString = "7,Пупкин,Иван,Сергеевич,+79001234567,@pup99,https://github.com/Testikpup"
+    val student = Student(studentString)
+    println(student)
+}
---
+++ LR2 Reading Class models/src/Student.kt
@@ -0,0 +1,125 @@
+package main.src
+
+class Student(
+    var id: Int,
+    var lastName: String,
+    var firstName: String,
+    var middleName: String,
+    var phone: String? = null,
+    var telegram: String? = null,
+    var email: String? = null,
+    var git: String? = null)
+{
+    init{
+        if(!isValidLastName(lastName) || lastName == ""){
+            throw IllegalArgumentException("Неправильный формат фамилия")
+        }
+        if(!isValidFirstName(firstName) || firstName == ""){
+            throw IllegalArgumentException("Неправильный формат имя")
+        }
+        if (phone != null && !isValidPhone(phone!!)) {
+            throw IllegalArgumentException("Неправильный формат телефона")
+        }
+    }
+
+    fun set_contacts(phone: String? = null, telegram: String? = null, email: String? = null, git: String? = null) {
+        if (phone != null) {
+            if (isValidPhone(phone)) {
+                this.phone = phone
+            } else {
+                throw IllegalArgumentException("Неправильный формат номера телефона")
+            }
+        }
+        if (telegram != null) {
+            if (isValidTelegram(telegram)) {
+                this.telegram = telegram
+            } else {
+                throw IllegalArgumentException("Неверный формат телеграма")
+            }
+        }
+        if (email != null) {
+            if (isValidEmail(email)) {
+                this.email = email
+            } else {
+                throw IllegalArgumentException("Неверный формат почты")
+            }
+        }
+        if (git != null) {
+            if (isValidGit(git)) {
+                this.git = git
+            } else {
+                throw IllegalArgumentException("Неверный формат гита")
+            }
+        }
+    }
+
+    fun getFullName(): String {
+        return "$lastName $firstName $middleName".trim()
+    }
+
+    override fun toString(): String {
+        return "Student(id=$id, fullName='${getFullName()}', phone=$phone, telegram=$telegram, email=$email, git=$git)"
+    }
+    // Проверка наличия гита
+    fun hasGit(): Boolean {
+        return git?.isNotBlank() == true
+    }
+    // Проверка наличия хотя бы одного контакта
+    fun hasContact(): Boolean {
+        return !phone.isNullOrBlank() || !telegram.isNullOrBlank() || !email.isNullOrBlank()
+    }
+    fun validate() : Boolean {
+        return hasGit() && hasContact()
+    }
+
+    constructor(studentArgs: HashMap<String,Any?>) : this(
+        id = studentArgs["id"] as Int,
+        firstName = studentArgs["firstName"].toString(),
+        lastName = studentArgs["lastName"].toString(),
+        middleName = studentArgs["middleName"].toString(),
+        phone = studentArgs.getOrDefault("phone",null) as String?,
+        telegram = studentArgs.getOrDefault("telegram",null) as String?,
+        email = studentArgs.getOrDefault("email",null) as String?,
+        git = studentArgs.getOrDefault("git",null) as String?
+    )
+
+    //Вот это новое. Принимает на вход строку, которую парсит
+    constructor(studentString: String) : this(
+        id = studentString.split(",")[0].toInt(),
+        lastName = studentString.split(",")[1],
+        firstName = studentString.split(",")[2],
+        middleName = studentString.split(",")[3],
+        phone = studentString.split(",").getOrNull(4),
+        telegram = studentString.split(",").getOrNull(5),
+        email = studentString.split(",").getOrNull(6),
+        git = studentString.split(",").getOrNull(7)
+    )
+
+    companion object Examination{
+        fun isValidPhone(phone: String): Boolean {
+            return phone.matches(Regex("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{10}$"))
+        }
+        fun isValidTelegram(telegram: String): Boolean {
+            return telegram.matches(Regex("^@[0-9a-zA-Z]{3,}$"))
+        }
+        fun isValidEmail(email: String): Boolean {
+            return email.matches(Regex("^([a-zA-Z0-9_-]{3,}@[a-zA-Z]+\\.[a-zA-Z]+)$"))
+        }
+        fun isValidGit(git: String): Boolean {
+            return git.matches(Regex("^https://github\\.com/[a-zA-Z0-9_-]+/?\$"))
+        }
+        fun isValidFirstName(firstName: String): Boolean {
+            return firstName.matches(Regex("^[А-ЯA-ZЁ]{1}[а-яa-zё]{2,}$"))
+        }
+        fun isValidLastName(lastName: String): Boolean {
+            return lastName.matches(Regex("^[А-ЯA-ZЁ]{1}[а-яa-zё]{2,}$"))
+        }
+        fun isValidMiddleName(middleName: String): Boolean {
+            return middleName.matches(Regex("^[А-ЯA-ZЁ]{1}[а-яa-zё]{2,}$"))
+        }
+
+        private fun validateFIO(lastName: String, firstName: String, middleName: String): Boolean {
+            return isValidLastName(lastName) && isValidFirstName(firstName) && isValidMiddleName(middleName)
+        }
+    }
+}
