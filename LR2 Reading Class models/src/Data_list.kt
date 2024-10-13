95d7be5a57561552fca8df5c192ca8799fce4949
Sc00taloo <v4nyapop0v12@gmail.com>
2024-10-13 22:26:57 +0300

    Task 2.4 - Данный класс должен реализовывать следующий методы: 4 метода + getValues

--- LR2 Reading Class models/src/Data_list.kt
+++ LR2 Reading Class models/src/Data_list.kt
@@ -1,2 +1,51 @@
 class Data_list<T : Comparable<T>>(private val data: MutableList<T> = mutableListOf()) {
+    private val selectedIndices = mutableSetOf<Int>() // хранит индексы выделенных элементов
+
+    // выделения элемента по номеру
+    fun select(number: Int) {
+        if (number in data.indices) {
+            selectedIndices.add(number)
+        }
+    }
+
+    // получение массив id выделенных элементов
+    fun get_selected(): List<Int> {
+        return selectedIndices.toList()
+    }
+
+    // получения имён атрибутов
+    fun get_names(): List<String> {
+        return if (data.isEmpty()) {
+            emptyList()
+        } else {
+            data.first()::class.java.declaredFields
+                .map { it.name }
+                .filter { it != "id" }
+        }
+    }
+
+    // получение данных в виде списка списков
+    fun getData(): List<List<Any>> {
+        return if (data.isEmpty()) {
+            emptyList()
+        } else {
+            val names = get_names().toMutableList().apply { add(0, "№") }
+            val values = data.mapIndexed { index, element ->
+                listOf(index + 1) + getValues(element)
+            }
+            listOf(names) + values
+        }
+    }
+
+    // получение значений атрибутов объекта
+    private fun getValues(element: T): List<Any> {
+        // все поля класса, кроме id
+        return element::class.java.declaredFields
+            .filter { it.name != "id" }
+            .map { field ->
+                field.isAccessible = true // делаем поле доступным
+                field.get(element)
+            }
+    }
+
 }
