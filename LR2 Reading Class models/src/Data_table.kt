f61e49df99df4d26ddd44a3ddcb3c4fef400fd31
Sc00taloo <v4nyapop0v12@gmail.com>
2024-10-13 17:31:04 +0300

    Task 2.2 - Реализовать методы, позволяющие – получить элемент по номеру строки и столбца БЕЗ возможности его редактировать, получить количество столбцов в таблице, получить количество строк в таблице.

--- LR2 Reading Class models/src/Data_table.kt
+++ LR2 Reading Class models/src/Data_table.kt
@@ -1,2 +1,18 @@
 class Data_table(private val data: Array<Array<Any>>) {
+    // Метод для получения количества строк
+    fun getRowCount(): Int {
+        return data.size
+    }
+    // Метод для получения количества столбцов
+    fun getColumnCount(): Int {
+        return if (data.isNotEmpty()) data[0].size else 0
+    }
+    // Метод для получения значения по индексу
+    fun getValue(row: Int, column: Int): Any? {
+        return if (row in data.indices && column in data[row].indices) {
+            data[row][column]
+        } else {
+            null // если индексы выходят за пределы
+        }
+    }
 }
