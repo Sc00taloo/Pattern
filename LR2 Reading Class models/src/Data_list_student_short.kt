3671ce5bb24b370531520e049df0c5e4309db8bc
Sc00taloo <v4nyapop0v12@gmail.com>
2024-10-15 13:25:04 +0300

    Task 2.5 - Создать наследника Data_list_student_short, реализующего методы get_data и get_names для класса Student_short.

--- LR2 Reading Class models/src/Data_list_student_short.kt
+++ LR2 Reading Class models/src/Data_list_student_short.kt
@@ -1,13 +1,10 @@
 import main.src.Student_short
 
 class Data_list_student_short(students: List<Student_short>) : Data_list<Student_short>(students) {
-
-    // Переопределяем метод для получения наименований атрибутов
     override fun get_names(): List<String> {
         return listOf("Фамилия и Инициалы", "git", "contact")
     }
-
-    // Переопределяем метод для создания данных
+    
     override fun get_data(): List<List<Any>> {
         val data = mutableListOf<List<Any>>()
 
@@ -15,7 +12,7 @@ class Data_list_student_short(students: List<Student_short>) : Data_list<Student
         for (index in super.get_selected()) {
             val student = data[index] // Получаем студента по индексу
             val row = listOf(
-                (index + 1).toString(), // № по порядку
+                (index + 1).toString(),
                 "${student}"
             )
             data.add(row)
