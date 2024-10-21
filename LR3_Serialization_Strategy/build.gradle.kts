cff64d225f02985efc7a1e60cb833a7548ac0249
Sc00taloo <v4nyapop0v12@gmail.com>
2024-10-22 00:08:19 +0300

    Task 3.3 - Реализовать класс Students_list_YAML, который будет работать с текстовым файлом, содержащим списки студентов.

--- LR3_Serialization_Strategy/build.gradle.kts
+++ LR3_Serialization_Strategy/build.gradle.kts
@@ -11,7 +11,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0") // Замените на актуальную версию
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("com.charleskorn.kaml:kaml:0.60.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
 }
 
 tasks.test {
--- LR3_Serialization_Strategy/src/main/kotlin/Main.kt
+++ LR3_Serialization_Strategy/src/main/kotlin/Main.kt
@@ -1,8 +1,4 @@
-import main.src.Student
-import main.src.Student_short
-import main.src.Students_list_txt
-import main.src.Students_list_JSON
-import main.src.SuperStudent
+import main.src.*
 
 fun main() {
 //    val input = "students.txt"
@@ -26,24 +22,44 @@ fun main() {
 //    studentsFromFile.removeStudentById(3)
 //    println(Student.students.toString())
 
-    val json = "student_json.json"
-    val studentsFromFileJSON = Students_list_JSON(json)
+//    val json = "student_json.json"
+//    val studentsFromFileJSON = Students_list_JSON(json)
+//
+//    println(studentsFromFileJSON.getStudent())
+//    println(studentsFromFileJSON.getStudentShortCount())
+//
+//    studentsFromFileJSON.sortStudents()
+//    println(studentsFromFileJSON.getStudent())
+//
+//    studentsFromFileJSON.getStudentById(1)
+//
+//    val student6 = Student(6,"Косян","Артём","Власович", phone = "+79214325674", git="https://github.com/Kosyan")
+//    studentsFromFileJSON.replaceStudentById(3, student6)
+//    println(studentsFromFileJSON.getStudent())
+//
+//    studentsFromFileJSON.writeToJson()
+//
+//    studentsFromFileJSON.removeStudentById(3)
+//    println(studentsFromFileJSON.getStudent())
 
-    println(studentsFromFileJSON.getStudent())
-    println(studentsFromFileJSON.getStudentShortCount())
 
-    studentsFromFileJSON.sortStudents()
-    println(studentsFromFileJSON.getStudent())
+    val yaml = "student_yaml.yaml"
+    val studentsFromFileYAML = Students_list_YAML(yaml)
 
-    studentsFromFileJSON.getStudentById(1)
+    println(studentsFromFileYAML.getStudent())
+    println(studentsFromFileYAML.getStudentShortCount())
+
+    studentsFromFileYAML.sortStudents()
+    println(studentsFromFileYAML.getStudent())
+
+    studentsFromFileYAML.getStudentById(1)
 
     val student6 = Student(6,"Косян","Артём","Власович", phone = "+79214325674", git="https://github.com/Kosyan")
-    studentsFromFileJSON.replaceStudentById(3, student6)
-    println(studentsFromFileJSON.getStudent())
+    studentsFromFileYAML.replaceStudentById(3, student6)
+    println(studentsFromFileYAML.getStudent())
 
-    studentsFromFileJSON.writeToJson()
+    studentsFromFileYAML.writeToYaml()
 
-    studentsFromFileJSON.removeStudentById(3)
-    println(studentsFromFileJSON.getStudent())
-
+    studentsFromFileYAML.removeStudentById(3)
+    println(studentsFromFileYAML.getStudent())
 }
