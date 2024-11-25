7aa37e2c4c2570a42530881201d13097138b4bae
Sc00taloo <v4nyapop0v12@gmail.com>
2024-11-26 02:12:28 +0300

    Task 4.4 - insert commands

---
+++ LR4_Postgresql/sql_commands/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO student (lastname, firstname, middlename, phone, telegram, email, git)
+VALUES ('Попов','Иван','Викторович','+78005553535','@scooty','iptru@mail.ru','https://github.com/Sc00taloo');
+
+INSERT INTO student (lastname, firstname, middlename, phone, telegram, email, git)
+VALUES ('Николай','Палыч','Победоносный','+79214326586','@nikitochka','sobaka12@gmail.com','https://github.com/SlivkiShow');
+
+INSERT INTO student (lastname, firstname, middlename, telegram, git)
+VALUES ('Денисова','Анастасия','Давидовна','@denis','https://github.com/Denisochka');
+
+INSERT INTO student (lastname, firstname, middlename, phone, telegram, email, git)
+VALUES ('Пономарёв','Максим','Максимович','+79451239009','@pomafyo123','pomafyo123@mail.ru', 'https://github.com/Killer2016');
+
+INSERT INTO student (lastname, firstname, middlename, phone, git)
+VALUES ('Косян','Артём','Власович','+79214325674','https://github.com/Kosyan');
