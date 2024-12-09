fdf98b513e0e077a826b656ed93986547a0c5a69
Sc00taloo <v4nyapop0v12@gmail.com>
2024-12-09 03:02:15 +0300

    Copy LR5 for LR6

---
+++ LR6_WIndow/src/main/kotlin/Data_list.kt
@@ -0,0 +1,28 @@
+import main.src.Student_short
+
+open class Data_list<T>(val data: List<Student_short> = mutableListOf()) {
+    private val selectedIndices = mutableSetOf<Int>()
+    // выделение элемента по номеру
+    fun select(number: Int) {
+        if (number in data.indices) {
+            selectedIndices.add(number)
+        } else {
+            throw IndexOutOfBoundsException("Недопустимый индекс: $number")
+        }
+    }
+
+    // получение id элементов
+    fun get_selected(): List<Int> {
+        return selectedIndices.toList()
+    }
+
+    // получение значений атрибутов объекта
+    open fun get_names(): List<String> {
+        throw IllegalArgumentException("Данный метод необходимо реализовать в классе наследнике")
+    }
+
+    // получение данных
+    open fun get_data(): List<List<Any>> {
+        throw IllegalArgumentException("Данный метод необходимо реализовать в классе наследнике")
+    }
+}
---
+++ LR6_WIndow/src/main/kotlin/SuperStudent.kt
@@ -0,0 +1,56 @@
+package main.src
+import java.io.File
+import java.lang.Exception
+
+open class SuperStudent(
+    var id: Int,
+    var lastName: String,
+    var firstName: String,
+    var middleName: String,
+    var phone: String? = null,
+    var telegram: String? = null,
+    var email: String? = null,
+    var git: String? = null
+) {
+    init {
+        if (!validate()) {
+            throw IllegalArgumentException("У студента должен быть хотя-бы один контакт или гит.")
+        }
+    }
+    // Проверка наличия гита
+    fun hasGit(): Boolean {
+        return git?.isNotBlank() == true
+    }
+    // Проверка наличия хотя бы одного контакта
+    fun hasContact(): Boolean {
+        return !phone.isNullOrBlank() || !telegram.isNullOrBlank() || !email.isNullOrBlank()
+    }
+    fun validate() : Boolean {
+        return hasGit() && hasContact()
+    }
+
+    fun FIO(): String {
+        val initials = "${firstName.first()}.${middleName.firstOrNull()?.toString() ?: ""}."
+        val tet = "$lastName $initials".trim()
+        return tet
+    }
+    fun contactMethod(): String{
+        val contact = when {
+            telegram != null -> "Telegram_$telegram"
+            email != null -> "Email_$email"
+            phone != null -> "Phone_$phone"
+            else -> "Нет доступных средств связи"
+        }
+        return contact
+    }
+    //Новое getInfo
+    fun getInfo(): String {
+        val fio = FIO()
+        val contact = contactMethod()
+        return "Инициаллы:$fio; Git:$git; Контакт:$contact"
+    }
+
+    override fun toString(): String {
+        return "Student(id=$id, fullName='${getInfo()}', phone=$phone, telegram=$telegram, email=$email, git=$git)"
+    }
+}
