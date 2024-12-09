7dbfd5a75440c1655c059a61cd270690267f5ff3
Sc00taloo <v4nyapop0v12@gmail.com>
2024-12-09 03:21:27 +0300

    Task 6.2-6.3 - Разработать приложение, содержащее три вкладки. Первая вкладка будет отображать список студентов, Student_list_view.

--- LR6_WIndow/src/main/java/MainWindow.java
+++ LR6_WIndow/src/main/java/MainWindow.java
@@ -1,16 +1,33 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainWindow {
     public void createWindow() {
-        JFrame frame = new JFrame("Java Window");
+        JFrame frame = new JFrame("Students");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
-        JLabel label = new JLabel("Тест", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 18));
+        JTabbedPane tabbedPane = new JTabbedPane();
 
-        frame.add(label);
+        // Первая вкладка
+        JPanel firstTab = new JPanel();
+        firstTab.add(new JLabel("Вкладка 1"));
+        tabbedPane.addTab("Student_list_view", firstTab);
+
+        // Вторая вкладка
+        JPanel secondTab = new JPanel();
+        secondTab.add(new JLabel("Вкладка 2"));
+        tabbedPane.addTab("Вкладка 2", secondTab);
+
+        // Третья вкладка
+        JPanel thirdTab = new JPanel();
+        thirdTab.add(new JLabel("Вкладка 3"));
+        tabbedPane.addTab("Вкладка 3", thirdTab);
+
+        frame.add(tabbedPane);
         frame.setVisible(true);
     }
 }
